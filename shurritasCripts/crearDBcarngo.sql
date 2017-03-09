bf375248457c907b926acd486454120e16db43be
mruwzum <daviddelatorre00@gmail.com>
2017-03-09 19:16:19 +0100

    actualizado persistance y script para nuevo nombre de la base de datos

--- shurritasCripts/crearDBcarngo.sql
+++ shurritasCripts/crearDBcarngo.sql
@@ -1,13 +1,13 @@
-drop database if exists `acme-carngo`;
-create database `acme-carngo`;
+drop database if exists `Acme-CnG`;
+create database `Acme-CnG`;
 drop user 'acme-user'@'%';
 drop user 'acme-manager'@'%';
 create user 'acme-user'@'%' identified by password '*4F10007AADA9EE3DBB2CC36575DFC6F4FDE27577';
 create user 'acme-manager'@'%' identified by password '*FDB8CD304EB2317D10C95D797A4BD7492560F55F';
 grant select, insert, update, delete 
-	on `acme-carngo`.* to 'acme-user'@'%';
+	on `Acme-CnG`.* to 'acme-user'@'%';
 
 grant select, insert, update, delete, create, drop, references, index, alter, 
         create temporary tables, lock tables, create view, create routine, 
         alter routine, execute, trigger, show view
-    on `acme-carngo`.* to 'acme-manager'@'%';
+    on `Acme-CnG`.* to 'acme-manager'@'%';
