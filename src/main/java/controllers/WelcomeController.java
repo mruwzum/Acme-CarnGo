cb09608fcdd35855015c3e5000d6ab0251ecdae5
mruwzum <daviddelatorre00@gmail.com>
2017-03-15 15:25:44 +0100

    banner

--- src/main/java/controllers/WelcomeController.java
+++ src/main/java/controllers/WelcomeController.java
@@ -11,7 +11,7 @@
 package controllers;
 
 import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.*;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -42,7 +42,16 @@ public class WelcomeController extends AbstractController {
 		result = new ModelAndView("welcome/index");
 		result.addObject("name", name);
 		result.addObject("moment", moment);
+		List<String> banner = new ArrayList<>();
+		banner.add(" <img src=\"http://www.ambientum.com/img_boletin/home/7-puntos-amovens.jpg\" alt=\"Smiley face\" height=\"400\" width=\"400\"> ");
+		banner.add(" <img src=\"http://blog.nasafcu.com/wp-content/uploads/2016/02/car_sharing_1.jpg\" alt=\"Smiley face\" height=\"242\" width=\"242\"> ");
+		banner.add(" <img src=\"http://www.altmedia.net.au/wp-content/uploads/2014/11/Silver_yaris_in_pod_car_share_bay_lower_res.jpeg\" alt=\"Smiley face\" height=\"242\" width=\"242\"> ");
+		banner.add(" <img src=\"http://www.businesstravellogue.com/files/2007/07/car-share.jpg\" alt=\"Smiley face\" height=\"242\" width=\"242\"> ");
 
+		Random randomGenerator = new Random();
+		int index = randomGenerator.nextInt(banner.size());
+		String bannerOut = banner.get(index);
+				result.addObject("banner",bannerOut);
 		return result;
 	}
 }
--- src/main/webapp/views/welcome/index.jsp
+++ src/main/webapp/views/welcome/index.jsp
@@ -18,4 +18,9 @@
 
 <p><spring:message code="welcome.greeting.prefix" /> ${name}<spring:message code="welcome.greeting.suffix" /></p>
 
-<p><spring:message code="welcome.greeting.current.time" /> ${moment}</p> 
+<p><spring:message code="welcome.greeting.current.time" /> ${moment}</p>
+
+
+<br>
+${banner}
+<br/>
