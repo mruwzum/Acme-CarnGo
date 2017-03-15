f71b928dc624b356013b102f93f9640621ed242c
mruwzum <daviddelatorre00@gmail.com>
2017-03-15 15:31:48 +0100

    banner

--- src/main/java/controllers/WelcomeController.java
+++ src/main/java/controllers/WelcomeController.java
@@ -35,23 +35,27 @@ public class WelcomeController extends AbstractController {
 		ModelAndView result;
 		SimpleDateFormat formatter;
 		String moment;
+		Random randomGenerator = new Random();
+
 
 		formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 		moment = formatter.format(new Date());
 
-		result = new ModelAndView("welcome/index");
-		result.addObject("name", name);
-		result.addObject("moment", moment);
+
 		List<String> banner = new ArrayList<>();
 		banner.add(" <img src=\"http://www.ambientum.com/img_boletin/home/7-puntos-amovens.jpg\" alt=\"Smiley face\" height=\"400\" width=\"400\"> ");
 		banner.add(" <img src=\"http://blog.nasafcu.com/wp-content/uploads/2016/02/car_sharing_1.jpg\" alt=\"Smiley face\" height=\"242\" width=\"242\"> ");
 		banner.add(" <img src=\"http://www.altmedia.net.au/wp-content/uploads/2014/11/Silver_yaris_in_pod_car_share_bay_lower_res.jpeg\" alt=\"Smiley face\" height=\"242\" width=\"242\"> ");
 		banner.add(" <img src=\"http://www.businesstravellogue.com/files/2007/07/car-share.jpg\" alt=\"Smiley face\" height=\"242\" width=\"242\"> ");
 
-		Random randomGenerator = new Random();
 		int index = randomGenerator.nextInt(banner.size());
 		String bannerOut = banner.get(index);
-				result.addObject("banner",bannerOut);
+
+
+		result = new ModelAndView("welcome/index");
+		result.addObject("name", name);
+		result.addObject("moment", moment);
+		result.addObject("banner",bannerOut);
 		return result;
 	}
 }
