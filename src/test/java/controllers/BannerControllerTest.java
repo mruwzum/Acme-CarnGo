8c57a45ab8bbe0640d7bc208d4cc4b829ed6bff9
mruwzum <daviddelatorre00@gmail.com>
2017-03-25 00:34:41 +0100

    controller test

--- src/test/java/controllers/BannerControllerTest.java
+++ src/test/java/controllers/BannerControllerTest.java
@@ -7,7 +7,6 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,7 +23,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:spring/datasource.xml",
         "classpath:spring/config/packages.xml",})
-@WebAppConfiguration
+//@WebAppConfiguration
 @Transactional
 public class BannerControllerTest extends AbstractTest {
 
